39ebe7c73ef911d504a6cbb7be31631ecb354fb5
alexnalfaro <93652575+alexnalfaro@users.noreply.github.com>
2022-10-07 12:38:41 +0200

    Add files via upload

--- Competicion.php
+++ Competicion.php
@@ -4,13 +4,36 @@
     </head>
     <body>
         <?php
-            class Competicion
+            class Competicion extends Corredor
             {
-                private $array = array();
+                private $corredores = array();
 
-                function anadirCarrera()
+                function anadirCorredor($corredor)
                 {
-                    
+
+                }
+                function anadirCarreraACorredor($idcorredor,$tiempo)
+                {   
+                    $nombre -> getNombre();
+                    $corredor = new Corredor($idcorredor,$nombre);
+                    $corredor->anadirCarrera($tiempo);
+                    array_push($corredores,$idcorredor,$corredor);
+                }
+                function mediaprimeraCarrera()
+                {
+
+                }
+                function corredormasrapido()
+                {
+
+                }
+                function corredores15seg()
+                {
+
+                }
+                function corredoresE()
+                {
+
                 }
             }
         ?>
